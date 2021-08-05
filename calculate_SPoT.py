e3459b45be6452e0c1d8e2c03669aa8f7b370ac1
Kevin Brick <kevbrick@gmail.com>
2021-08-05 13:46:39 -0400

    Change outputs to .txt

--- calculate_SPoT.py
+++ calculate_SPoT.py
@@ -7,7 +7,7 @@ import sys
 import math
 
 parser = argparse.ArgumentParser(
-    description="Calculate the fraction of reads in peaks (SPoT)."
+    description="Calculate the signal portion of tags / fragments (SPoT)."
 )
 
 ## Allow "Just show version" mode
@@ -16,6 +16,8 @@ parser.add_argument("--intervals_bed", help="REV fragments BED")
 parser.add_argument("--name", help="Reads name")
 parser.add_argument("--iname", help="Intervals name")
 parser.add_argument("--g", help="Genome index file")
+parser.add_argument("--o", default="no_name", help="Output prefix")
+parser.add_argument("--norand", action="store_false", help="No randomized set")
 parser.add_argument("--vers", action="store_true", help="Echo version")
 parser.add_argument("--v", action="store_true", help="Verbose mode")
 
@@ -31,15 +33,15 @@ rand_intervals_bed = (
     BedTool(args.intervals_bed).shuffle(g=args.g, chrom=True, seed=42).sort()
 )
 
-reads_bed.saveas("x.bed")
-intervals_bed.saveas("i.bed")
-rand_intervals_bed.saveas("r.bed")
+out_report = open(args.prefix + ".SSDS_SPoT_report.txt", "w")
 
 at_intervals = reads_bed.intersect(b=intervals_bed, u=True, sorted=True)
-at_random = reads_bed.intersect(b=rand_intervals_bed, u=True, sorted=True)
-
 interval_SPoT = round(float(at_intervals.count()) / float(reads_bed.count()), 3)
-random_SPoT = round(float(at_random.count()) / float(reads_bed.count()), 3)
+out_report.print(args.name + "_SPoT\t" + args.iname + "\t" + str(interval_SPoT))
 
-print(args.name + "_SPoT\t" + args.iname + "\t" + str(interval_SPoT))
-print(args.name + "_SPoT\t" + args.iname + "(R)" + "\t" + str(random_SPoT))
+if not args.norand:
+    at_random = reads_bed.intersect(b=rand_intervals_bed, u=True, sorted=True)
+    random_SPoT = round(float(at_random.count()) / float(reads_bed.count()), 3)
+    out_report.print(
+        args.name + "_SPoT\t" + args.iname + "(R)" + "\t" + str(random_SPoT)
+    )
--- parse_SSDS_BAM.py
+++ parse_SSDS_BAM.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from collections import defaultdict
 from Bio.Seq import Seq
